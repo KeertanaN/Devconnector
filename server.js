5ed774459091ab05442ff0a3ede364fd157a3f0e
KeertanaN <keertana_1@yahoo.co.in>
2019-09-11 16:12:29 -0700

    Adding post, profile,users,key files

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 const app = express();
 
 //Db config
 const db = require('./config/keys').mongoURI;
 //connect to mongo db
-mongoose.connect(db)
+mongoose.connect(db, { useNewUrlParser: true })
         .then(()=>console.log('MongoDB connected'))
         .catch(err=>console.log(err));
 
 app.get('/',(req,res)=>res.send('Hello World!! and good bye'));
+
+app.use('/api/users',users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 const port = 7000;
 app.listen(port,()=> console.log(`server running on port ${port}`));
