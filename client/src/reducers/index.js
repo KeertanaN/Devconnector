039e71fdb47426029eb3908bf3a41334fc701633
KeertanaN <keertana_1@yahoo.co.in>
2019-10-24 23:12:10 -0700

    maed changes to profile.js and index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducers from './authReducer';
 import errorReducer from './errorReducer';
+import profileReducer from './profileReducer';
  
 export default combineReducers({
   auth: authReducers,
-  errors: errorReducer
+  errors: errorReducer,
+  profile: profileReducer
 });
